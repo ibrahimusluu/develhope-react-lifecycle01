84c183512c2134cb12dc08b852f00bd99ecaf3cb
İbrahim Uslu <ibrahim069612@gmail.com>
2023-01-06 08:34:55 +0300

    componentDidMount Lifecycle method used

---
+++ src/Counter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { CounterDisplay } from "./CounterDisplay";
+// initial value of the counter, the increment interval and the increment amount are passed as props
+export class Counter extends React.Component {
+  state = {
+    count: this.props.initialValue,
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState((state) => {
+        return {
+          count: state.count + this.props.incrementAmount,
+        };
+      });
+    }, this.props.incrementInterval);
+  }
+
+  render() {
+    return (
+      <div>
+        <CounterDisplay count={this.state.count} />
+      </div>
+    );
+  }
+}
